120f94e0d3b32772257c03920e6f107f4663fdb0
chris.fisher <christopher.fisher@conduent.com>
2022-07-14 10:13:32 +0100

    added dependancy fix

--- PageObjects/Permit/PermitLandingPage.py
+++ PageObjects/Permit/PermitLandingPage.py
@@ -7,17 +7,18 @@ from PageObjects.Permit.PermitVehiclePage import PermitVehicle
 
 class PermitLanding:
     driver: WebDriver
+    page_title_selector = ".Title"
 
     def __init__(self, driver) -> None:
         super().__init__()
         self.driver = driver
         WebDriverWait(self.driver, 30).until(
             EC.text_to_be_present_in_element(
-                (By.CSS_SELECTOR, ".Title"), "Zero Emission Zone (ZEZ)"))
+                (By.CSS_SELECTOR, self.page_title_selector), "Zero Emission Zone (ZEZ)"))
 
     def is_page_tile_displayed(self) -> bool:
-        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".Title")))
-        return self.driver.find_element(By.CSS_SELECTOR, ".Title").text == "Zero Emission Zone (ZEZ)"
+        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, self.page_title_selector)))
+        return self.driver.find_element(By.CSS_SELECTOR, self.page_title_selector).text == "Zero Emission Zone (ZEZ)"
 
     def click_next(self) -> PermitVehicle:
         self.driver.find_element(By.CSS_SELECTOR, ".BoxedText").click()
